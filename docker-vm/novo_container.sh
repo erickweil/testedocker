c7c198e66d9f79a64d5ed4e3928db75879ba871b
Erick <erick.weil@ifro.edu.br>
2023-08-31 22:08:29 -0400

    Reorganizado para utilizar docker compose

--- docker-vm/novo_container.sh
+++ docker-vm/novo_container.sh
@@ -1,40 +1,25 @@
 #!/bin/bash
-# Script a ser executado na máquina com docker e sysbox instalado
-# para iniciar um container com usuário criado
 export USERNAME=$1
 export PASSWORD=$2
-export CODEPORT=$3
-export SSHPORT=$4
-export CONTAINER=$USERNAME
 
-docker network create net-vm
+export USER_CONTAINER_IMAGE="erickweil/container-vm:code-ptbr"
+export DOCKER_NETWORK="alunos"
 
-# A fazer: Limitar o tamanho do volume
-# https://github.com/moby/moby/issues/16670
-# https://github.com/moby/moby/issues/41328
-docker volume create vol-$USERNAME
+echo "Criando arquivos de configuração"
+mkdir -p "./usuarios/$USERNAME"
 
-# https://docs.docker.com/engine/reference/run/
-# A opção --storage-opt não afeta os volumes, apenas dados salvos
-# no sistema de arquivos overlay do próprio container. o valor padrão é 10GB e só funciona em alguns sistemas de arquivos
-# A opção --memory-reservation deve sempre ser menor que --memory e serve apenas
-# para dar uma 'dica' para o container tentar usar menos memória.
-docker run -d --runtime=sysbox-runc \
-	--name $CONTAINER \
-	-v vol-$USERNAME:/home/$USERNAME \
-	--network net-vm \
-	--restart=unless-stopped \
-	-p $CODEPORT:8080 \
-	-p $SSHPORT:22 \
-	--memory="2G" \
-	--memory-reservation="1G" \
-	--cpus=1 \
-	erickweil/container-vm
+ENVFILE="./usuarios/$USERNAME/.env"
+envsubst < ".env.template" > $ENVFILE
 
-# Obs:
-# 2G de memória não foi suficiente para rodar os testes
-# quando em paralelo. lembrar da opção do jest para rodar 
-# um por vez --runInBand
+COMPOSEFILE="./usuarios/$USERNAME/docker-compose.yml"
+cp "./usuarios/docker-compose.yml" $COMPOSEFILE
 
-# Cria o usuário, configura o code-server, git, chave ssh, node, e permissões do /home do usuário
-docker exec $CONTAINER bash -c "bash /root/create_user.sh $USERNAME $PASSWORD"
+SQLFILE="./usuarios/$USERNAME/create_user.sql"
+envsubst '${USERNAME} ${PASSWORD}' < "create_user.template.sql" > $SQLFILE
+#sed -i -e "s/\-/\\\-/g" $SQLFILE
+
+echo "Criando usuário no banco de dados"
+mysql -h 127.0.0.1 < $SQLFILE
+
+echo "Iniciando o container"
+bash ./subir_container.sh $COMPOSEFILE $ENVFILE up
--- mysql/scripts/create_user.template.sql
+++ mysql/scripts/create_user.template.sql
@@ -1,11 +1,12 @@
-CREATE USER '${USUARIO}'@'%' IDENTIFIED BY '${SENHA}';
--- https://stackoverflow.com/questions/5988842/can-i-grant-access-to-databases-with-prefix-then-wild-card
-GRANT ALL PRIVILEGES ON `${USUARIO}\_%`.* TO '${USUARIO}'@'%';
+CREATE USER IF NOT EXISTS `${USERNAME}`@'%' IDENTIFIED BY '${PASSWORD}';
+GRANT ALL PRIVILEGES ON `${USERNAME}\_%`.* TO `${USERNAME}`@'%';
 
-CREATE DATABASE IF NOT EXISTS ${USUARIO}_banco;
-USE ${USUARIO}_banco;
+CREATE DATABASE IF NOT EXISTS `${USERNAME}_banco`;
+USE `${USERNAME}_banco`;
 
-CREATE TABLE exemplo (id INT, name VARCHAR(20), email VARCHAR(20));
-INSERT INTO exemplo (id,name,email) VALUES(1,"${USUARIO}","${USUARIO}@abc.com");
-INSERT INTO exemplo (id,name,email) VALUES(2,"Exemplo 2","exemplo2@gmail.com");
-INSERT INTO exemplo (id,name,email) VALUES(3,"Exemplo 3","exemplo3@yahoo.com");
+CREATE TABLE IF NOT EXISTS exemplo (
+	id INT PRIMARY KEY,
+	name VARCHAR(255),
+	email VARCHAR(255)
+);
+REPLACE INTO exemplo (id,name,email) VALUES(1,"exemplo","exemplo@abc.com");
